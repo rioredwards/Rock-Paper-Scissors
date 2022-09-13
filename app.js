a1d8ee20129acb941330eb2966fff648e136093c
Rio Edwards <rioredwards@gmail.com>
2022-09-13 16:08:07 -0700

    Start on app.js. Add DOM variables for move gamestate

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 /* Imports */
 
 /* State */
+let gameState = 'move'; // 'move' or 'results'
 
 /* Actions */
-function loadPage() {}
-
-/* Components */
+function loadPage() {
+    displayMove();
+    // displayResults();
+    // displayScoreboard();
+}
 
 /* Component */
 // get DOM
+const gameStateText = document.getElementById('game-state-text');
+const rockBtn = document.getElementById('rockMove');
+const paperBtn = document.getElementById('paperMove');
+const scissorsBtn = document.getElementById('scissorsMove');
+
 // display
+function displayMove() {}
+// function displayResults() {} TODO
+// function displayScoreboard() {} TODO
+
 // event listeners
 
 /* Run page load code */
 loadPage();
+logDOM();
+
+/* Logging */
+function logDOM() {
+    console.log('gameStateText: ', gameStateText);
+    console.log('rockBtn: ', rockBtn);
+    console.log('paperBtn: ', paperBtn);
+    console.log('scissorsBtn: ', scissorsBtn);
+}
