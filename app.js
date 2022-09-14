3fd697e59aff6136e06cf3486a8b1638037147f1
Rio Edwards <rioredwards@gmail.com>
2022-09-13 17:20:08 -0700

    Add more pseudocode. Create array of objects for selections.

--- app.js
+++ app.js
@@ -2,6 +2,24 @@
 
 /* State */
 let gameState = 'move'; // 'move' or 'results'
+let userSelection; // rock, paper or scissors
+let compSelection; // rock, paper or scissors
+let result; // 'win', 'lose'
+
+const selections = [
+    {
+        value: 'rock',
+        beats: 'scissors',
+    },
+    {
+        value: 'paper',
+        beats: 'rock',
+    },
+    {
+        value: 'scissors',
+        beats: 'paper',
+    },
+];
 
 /* Actions */
 function loadPage() {
@@ -10,12 +28,18 @@ function loadPage() {
     // displayScoreboard();
 }
 
+/* function compMove() {
+    const randNum = Math.floor(Math.random() * 3);
+} */
+
 /* Component */
 // get DOM
 const gameStateText = document.getElementById('game-state-text');
 const rockBtn = document.getElementById('rockMove');
 const paperBtn = document.getElementById('paperMove');
 const scissorsBtn = document.getElementById('scissorsMove');
+
+const playAgainBtn = document.getElementById('playAgainButton');
 
 // display
 function displayMove() {}
@@ -25,14 +49,42 @@ function displayMove() {}
 // event listeners
 rockBtn.addEventListener('click', () => {
     console.log('rock click');
+    userSelection = selections[0];
+    // compSelection = compMove();
+    logSelections();
+    gameState = 'results';
+    // result = calcResult();
+    // updateScore(result);
+    loadPage();
 });
 
 paperBtn.addEventListener('click', () => {
     console.log('paper click');
+    userSelection = selections[1];
+    // compSelection = compMove();
+    logSelections();
+    gameState = 'results';
+    // result = calcResult();
+    // updateScore(result);
+    loadPage();
 });
 
 scissorsBtn.addEventListener('click', () => {
     console.log('scissors click');
+    userSelection = selections[2];
+    // compSelection = compMove();
+    logSelections();
+    gameState = 'results';
+    // result = calcResult();
+    // updateScore(result);
+    loadPage();
+});
+
+playAgainBtn.addEventListener('click', () => {
+    console.log('play again click');
+    gameState = 'move';
+    // resetMove();
+    loadPage();
 });
 
 /* Run page load code */
@@ -45,4 +97,9 @@ function logDOM() {
     console.log('rockBtn: ', rockBtn);
     console.log('paperBtn: ', paperBtn);
     console.log('scissorsBtn: ', scissorsBtn);
+}
+
+function logSelections() {
+    console.log('userSelection: ', userSelection);
+    console.log('compSelection: ', compSelection);
 }
